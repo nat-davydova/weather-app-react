7f36e37fbb167d3bffc1fc8ead2b7bc85a6491eb
nat-davydova <natti.davydova@gmail.com>
2019-10-23 17:51:56 +0300

    frame: multiple classes added

--- weather-app-react/src/components/Frame/Frame.js
+++ weather-app-react/src/components/Frame/Frame.js
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Frame.module.scss';
 
 const frame = (props) => (
-	<div className={classes.frame}>
+	<div className={`${classes.frame} container`}>
 		{props.children}
 	</div>
 );
