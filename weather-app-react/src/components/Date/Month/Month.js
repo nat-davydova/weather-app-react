126de2b7555d88fc59c6ccc817faa1c1997049d6
nat-davydova <natti.davydova@gmail.com>
2019-10-29 19:12:56 +0300

    Date: month component - added comma

--- weather-app-react/src/components/Date/Month/Month.js
+++ weather-app-react/src/components/Date/Month/Month.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import classes from './Month.module.scss';
+
 const month = (props) => {
 
 	const { month } = props;
 
-	return <div className="month">{month}</div>
+	return <div className={classes.month}>{month}</div>
 
 };
 
