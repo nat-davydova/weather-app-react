129a87a6aa3c6d8c6b9f1cff8e1442e4f0c793b4
nat-davydova <natti.davydova@gmail.com>
2019-10-29 18:55:38 +0300

    Date: basic structure

--- weather-app-react/src/components/Date/Date.js
+++ weather-app-react/src/components/Date/Date.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import Row from 'components/Miscellanious/Row/Row';
 import Col from 'components/Miscellanious/Col/Col';
 
 const date = (props) => {
 
-	return null
+	const dateClasses = classnames(
+		`text-center`,
+		`d-flex`,
+		`justify-content-center`
+	);
+
+	return(
+
+		<Row>
+			<Col colWidth={12}>
+
+				<div className={dateClasses}>
+
+				</div>
+
+			</Col>
+		</Row>
+
+	);
 
 };
 
