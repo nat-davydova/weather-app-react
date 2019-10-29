5fac8c018c343bfb59e2d1370d0cc5ad3be823c4
nat-davydova <natti.davydova@gmail.com>
2019-10-29 18:34:46 +0300

    Row component

---
+++ weather-app-react/src/components/Miscellanious/Row/Row.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const row = (props) => <div className="row">{props.children}</div>;
+
+export default row;
--- weather-app-react/src/containers/Home/Home.js
+++ weather-app-react/src/containers/Home/Home.js
@@ -7,6 +7,7 @@ class Home extends Component{
 		return(
 			<Fragment>
 
+
 			</Fragment>
 		);
 	}
