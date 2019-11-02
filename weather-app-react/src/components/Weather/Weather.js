b11f5424bedac58812958fe322c3347f97d0e12a
nat-davydova <natti.davydova@gmail.com>
2019-11-02 19:00:45 +0300

    Weather: basic structure and preloader

--- weather-app-react/src/components/Weather/Weather.js
+++ weather-app-react/src/components/Weather/Weather.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import classnames from 'classnames';
+
+import Row from 'components/Miscellanious/Row/Row';
+import Col from 'components/Miscellanious/Col/Col';
+import Preloader from 'components/Preloader/Preloader';
 
 const weather = (props) => {
 
-	return null
+	const { weather } = props;
+
+	const weatherClasses = classnames(
+		'mt-3',
+		'mt-sm-5',
+		'd-flex',
+		'justify-content-center'
+	);
+
+	return(
+		<Row>
+			<Col colWidth={12}>
+				<div className={weatherClasses}>
+
+					{weather ? null : <Preloader iconSize={`md`} color={`text-success`}/>}
+
+				</div>
+			</Col>
+		</Row>
+	);
 
 };
 
