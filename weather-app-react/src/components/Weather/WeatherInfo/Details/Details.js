5b55e3336179fda30504e5b1a516bd236a31a0e5
nat-davydova <natti.davydova@gmail.com>
2019-11-04 21:32:12 +0300

    Weather: details - structure and  styles

--- weather-app-react/src/components/Weather/WeatherInfo/Details/Details.js
+++ weather-app-react/src/components/Weather/WeatherInfo/Details/Details.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import classnames from 'classnames';
+
+import classes from './Details.module.scss';
 
 const details = (props) => {
-	
-	return(
 
+	const detailsListClasses = classnames(
+		classes.details_list,
+		'p-0'
+	);
+
+	return(
+		<div className="mt-4">
+			<ul className={detailsListClasses}>
+
+			</ul>
+		</div>
 	);
 };
 
