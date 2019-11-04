06d7f3a00f8d80d2495acd76e7f412e4d466c67e
nat-davydova <natti.davydova@gmail.com>
2019-11-04 16:03:29 +0300

    Weather: object of weather icons

--- weather-app-react/src/components/Weather/WeatherInfo/Icon/Icon.js
+++ weather-app-react/src/components/Weather/WeatherInfo/Icon/Icon.js
@@ -2,82 +2,39 @@ import React from 'react';
 
 import Icon from 'components/Miscellanious/Icon/Icon';
 
+import { ReactComponent as BrokenCloudsIcon } from 'assets/img/weather/broken-clouds.svg';
+import { ReactComponent as ClearSkyIcon } from 'assets/img/weather/clear-sky.svg';
+import { ReactComponent as ClearSkyNightIcon } from 'assets/img/weather/clear-sky-night.svg';
+import { ReactComponent as FewCloudsIcon } from 'assets/img/weather/few-clouds.svg';
+import { ReactComponent as FewCloudsNightIcon } from 'assets/img/weather/few-clouds-night.svg';
 import { ReactComponent as MistIcon } from 'assets/img/weather/mist.svg';
+import { ReactComponent as RainIcon } from 'assets/img/weather/rain.svg';
+import { ReactComponent as ScatteredCloudsIcon } from 'assets/img/weather/scattered-clouds.svg';
+import { ReactComponent as ShowerRainIcon } from 'assets/img/weather/shower-rain.svg';
+import { ReactComponent as SnowIcon } from 'assets/img/weather/snow.svg';
+import { ReactComponent as ThermometerIcon } from 'assets/img/weather/thermometer.svg';
+import { ReactComponent as ThunderstormIcon } from 'assets/img/weather/thunderstorm.svg';
 
-//pick weather icon
-const weatherIcon = (weather, localHours, sunset, sunrise) => {
-
-	let icon;
-
-	switch(weather) {
-
-		case 'Thunderstorm':
-			icon = 'thunderstorm';
-			break;
-
-		case 'Rain':
-			icon = 'rain';
-			break;
-
-		case 'Shower Rain':
-			icon = 'shower-rain';
-			break;
-
-		case 'Snow':
-			icon = 'snow';
-			break;
-
-		case 'Fog':
-			icon = 'mist';
-			break;
-
-		case 'Mist':
-			icon = 'mist';
-			break;
-
-		case 'Clear Sky':
-			if (localHours < sunset && localHours > sunrise) {
-				icon = 'clear-sky'
-			} else {
-				icon = 'clear-sky-night'
-			}
-			break;
-
-		case 'Few Clouds':
-			if (localHours < sunset && localHours > sunrise) {
-				icon = 'few-clouds'
-			} else {
-				icon = 'few-clouds-night'
-			}
-			break;
-
-		case 'Scattered Clouds':
-			icon = 'scattered-clouds';
-			break;
-
-		case 'Broken Clouds':
-			icon = 'broken-clouds';
-			break;
-
-		default:
-			icon = 'thermometer';
-			break;
-	}
-
-
-
-	return icon.charAt(0).toUpperCase() + icon.slice(1);
-
+//weather icons set
+const weatherIcons = {
+	brokenClouds: BrokenCloudsIcon,
+	clearSky: ClearSkyIcon,
+	clearSkyNight: ClearSkyNightIcon,
+	default: ThermometerIcon,
+	fewClouds: FewCloudsIcon,
+	fewCloudsNight: FewCloudsNightIcon,
+	fog: MistIcon,
+	mist: MistIcon,
+	rain: RainIcon,
+	scatteredClouds: ScatteredCloudsIcon,
+	showerRain: ShowerRainIcon,
+	snow: SnowIcon,
+	thunderstorm: ThunderstormIcon,
 };
 
 const icon = (props) => {
 
 	const { weatherType } = props;
-
-	const iconName = weatherIcon(weatherType, 20, 8, 17);
-
-	const WeatherIconName = `${iconName}Icon`;
-
 
 	return(
 
