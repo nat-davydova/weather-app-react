79618a211f63a148b696230c60ec9fd0cd193105
nat-davydova <natti.davydova@gmail.com>
2019-11-07 21:48:24 +0300

    Refactoring: error component improvement

--- weather-app-react/src/components/Miscellanious/Error/Error.js
+++ weather-app-react/src/components/Miscellanious/Error/Error.js
@@ -1,10 +1,5 @@
 import React from 'react';
 
-const error = (props) => {
-
-	const { errorText } = props;
-
-	return <div className="text-danger">{errorText}</div>
-};
+const error = ({ errorText }) => <div className="text-danger">{errorText}</div>;
 
 export default error;
